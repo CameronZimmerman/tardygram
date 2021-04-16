6f17eb4feb2f7ffff9251cc881497a95b2b96706
Cameron A Zimmerman <cameronazimmerman@gmail.com>
2021-04-16 16:13:10 -0700

    passing test for for delete posts

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,13 +6,13 @@ const User = require('../lib/models/User');
 
 const user = {
   username: 'test-user',
-  photoUrl: '/some-image.jpg'
+  photoUrl: '/some-image.jpg',
 };
 
 const post = {
   photoUrl: '/some-image.jpg',
   caption: 'new image',
-  tags: ['one', 'two', 'three']
+  tags: ['one', 'two', 'three'],
 };
 
 const comment = {
@@ -29,8 +29,7 @@ jest.mock('../lib/middleware/ensureAuth.js', () => (req, res, next) => {
   req.user = {
     username: 'test-user',
     photo_url: 'http:/some-image.jpg',
-
-  }
+  };
   next();
 });
 describe('Tardygram routes', () => {
@@ -38,8 +37,7 @@ describe('Tardygram routes', () => {
     return setup(pool);
   });
 
-
-  //post tests 
+  //post tests
   it('inserts a post into the database via POST', async () => {
     await User.insert(user);
 
@@ -48,117 +46,111 @@ describe('Tardygram routes', () => {
       .send({
         photoUrl: '/some-image.jpg',
         caption: 'new image',
-        tags: ['one', 'two', 'three']
+        tags: ['one', 'two', 'three'],
       })
       .then((res) => {
-        expect(res.body).toEqual({id: "1", username: 'test-user', photoUrl: '/some-image.jpg', caption: 'new image', tags: ['one', 'two', 'three']})
-      })
-  })
+        expect(res.body).toEqual({
+          id: '1',
+          username: 'test-user',
+          photoUrl: '/some-image.jpg',
+          caption: 'new image',
+          tags: ['one', 'two', 'three'],
+        });
+      });
+  });
 
   it('gets all posts', async () => {
     await User.insert(user);
-    await request(app)
-      .post('/api/v1/posts')
-      .send(post)
+    await request(app).post('/api/v1/posts').send(post);
 
     return request(app)
       .get('/api/v1/posts')
       .then((res) => {
-        expect(res.body).toEqual([{id: "1", username: 'test-user', photoUrl: '/some-image.jpg', caption: 'new image', tags: ['one', 'two', 'three']}])
-      })
-  })
+        expect(res.body).toEqual([
+          {
+            id: '1',
+            username: 'test-user',
+            photoUrl: '/some-image.jpg',
+            caption: 'new image',
+            tags: ['one', 'two', 'three'],
+          },
+        ]);
+      });
+  });
 
   it('gets a posts when given an id', async () => {
     await User.insert(user);
 
-    await request(app)
-      .post('/api/v1/posts')
-      .send(post)
+    await request(app).post('/api/v1/posts').send(post);
 
     await request(app)
       .post('/api/v1/comments')
       .send(comment)
-      .then(console.log(comment))
+      .then(console.log(comment));
 
     return request(app)
       .get('/api/v1/posts/1')
       .then((res) => {
-        expect(res.body).toEqual(
-          {
-            caption: "new image",
-            comments: ["this is a comment"],
-            id: "1",
-            photoUrl: "/some-image.jpg",
-            tags: ["one", "two", "three"],
-            username: "test-user"
-          }
-        )
-      })
-  })
+        expect(res.body).toEqual({
+          caption: 'new image',
+          comments: ['this is a comment'],
+          id: '1',
+          photoUrl: '/some-image.jpg',
+          tags: ['one', 'two', 'three'],
+          username: 'test-user',
+        });
+      });
+  });
 
   it('updates a posts caption when given an id', async () => {
     await User.insert(user);
 
-    await request(app)
-      .post('/api/v1/posts')
-      .send(post)
+    await request(app).post('/api/v1/posts').send(post);
 
     await request(app)
       .post('/api/v1/comments')
       .send(comment)
-      .then(console.log(comment))
+      .then(console.log(comment));
 
     return request(app)
       .patch('/api/v1/posts/1')
-      .send({caption: "this is my new caption!!!!!"})
+      .send({ caption: 'this is my new caption!!!!!' })
       .then((res) => {
-        expect(res.body).toEqual(
-          {
-            caption: "this is my new caption!!!!!",
-            id: "1",
-            photoUrl: "/some-image.jpg",
-            tags: ["one", "two", "three"],
-            username: "test-user"
-          }
-        )
-      })
-  })
+        expect(res.body).toEqual({
+          caption: 'this is my new caption!!!!!',
+          id: '1',
+          photoUrl: '/some-image.jpg',
+          tags: ['one', 'two', 'three'],
+          username: 'test-user',
+        });
+      });
+  });
 
   it('deletes a post from the database when given an idea via DELETE', async () => {
     await User.insert(user);
 
-    await request(app)
-      .post('/api/v1/posts')
-      .send(post)
+    await request(app).post('/api/v1/posts').send(post);
 
-    await request(app)
-      .post('/api/v1/comments')
-      .send(comment)
-
-    await request(app)
-      .post('/api/v1/comments')
-      .send(comment2)
+    await request(app).post('/api/v1/comments').send(comment);
 
     return request(app)
-      .delete('/api/v1/post/1')
+      .delete('/api/v1/posts/1')
       .then((res) => {
         expect(res.body).toEqual({
-          caption: "new image",
-          id: "1",
-          photoUrl: "/some-image.jpg",
-          tags: ["one", "two", "three"],
-          username: "test-user"
-        })
-      })
-  })
+          caption: 'new image',
+          id: '1',
+          photoUrl: '/some-image.jpg',
+          tags: ['one', 'two', 'three'],
+          username: 'test-user',
+        });
+      });
+  });
 
   // comment tests
   it('inserts a comment into the database via POST', async () => {
     await User.insert(user);
 
-    await request(app)
-      .post('/api/v1/posts')
-      .send(post)
+    await request(app).post('/api/v1/posts').send(post);
 
     return request(app)
       .post('/api/v1/comments')
@@ -167,31 +159,33 @@ describe('Tardygram routes', () => {
         post: '1',
       })
       .then((res) => {
-        expect(res.body).toEqual({id: '1', post: '1', commentBy: 'test-user', comment: 'this is a comment'})
-      })
-  })
+        expect(res.body).toEqual({
+          id: '1',
+          post: '1',
+          commentBy: 'test-user',
+          comment: 'this is a comment',
+        });
+      });
+  });
 
   it('deletes a comment from the database when given an idea via DELETE', async () => {
     await User.insert(user);
 
-    await request(app)
-      .post('/api/v1/posts')
-      .send(post)
+    await request(app).post('/api/v1/posts').send(post);
 
-    await request(app)
-      .post('/api/v1/comments')
-      .send(comment)
+    await request(app).post('/api/v1/comments').send(comment);
 
-    await request(app)
-      .post('/api/v1/comments')
-      .send(comment2)
+    await request(app).post('/api/v1/comments').send(comment2);
 
     return request(app)
       .delete('/api/v1/comments/1')
       .then((res) => {
-        expect(res.body).toEqual({id: '1', post: '1', commentBy: 'test-user', comment: 'this is a comment'})
-      })
-  })
-
-
+        expect(res.body).toEqual({
+          id: '1',
+          post: '1',
+          commentBy: 'test-user',
+          comment: 'this is a comment',
+        });
+      });
+  });
 });
