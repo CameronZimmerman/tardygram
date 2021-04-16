0de5eca58762e1640328f81b872b8b915d09b5d2
Dominique Sakrisson <domsakrisson@gmailcom>
2021-04-16 09:08:13 -0700

    POST MODEL AND TES

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,5 +1,20 @@
 drop table if exists users cascade;
+drop table if exists posts;
+drop table if exists comments;
 create table users(
   github_username text not null primary key,
   github_photo_url text not null
+);
+create table posts(
+id bigint generated always as identity primary key,
+username text not null references users(github_username),
+photo_url text not null,
+caption text,
+tags text[]
+);
+create table comments(
+id bigint generated always as identity primary key,
+comment_by text not null references users(github_username),
+post text not null references posts(id),
+comment text not null
 );
