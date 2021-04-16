7e182f00412835d636ad559b384e755982683515
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-04-16 12:36:16 -0400

    posts post route test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,11 +5,11 @@ const app = require('../lib/app');
 const User = require('../lib/models/User');
 
 // jest.useFakeTimers()
-jest.mock('../lib/middleware/ensureAuth.js',() => (req,res,next) => {
+jest.mock('../lib/middleware/ensureAuth.js', () => (req, res, next) => {
   req.user = {
     username: 'test-user',
     photo_url: 'http:/some-image.jpg',
-    
+
   }
   next();
 });
@@ -19,17 +19,22 @@ describe('Tardygram routes', () => {
   });
 
 
-  it('inserts a post into the database via POST', () => {
-    return request(app)
-    .post('/api/v1/auth/posts')
-    .send({ 
-      username: 'test-user', 
-      photoUrl: '/some-image.jpg', 
-      caption:'new image',  
-      tags: ['one', 'two', 'three']})
-    .then((res) => {
-      expect(res.body).toEqual({ id: 1, username: 'test-user', photoUrl: '/some-image.jpg', caption:'new image',  tags: ['one', 'two', 'three']})
+  it('inserts a post into the database via POST', async () => {
+    const user = await User.insert({
+      username: 'test-user',
+      photoUrl: '/some-image.jpg'
     })
+    return request(app)
+      .post('/api/v1/posts')
+      .send({
+        // username: 'test-user',
+        photoUrl: '/some-image.jpg',
+        caption: 'new image',
+        tags: ['one', 'two', 'three']
+      })
+      .then((res) => {
+        expect(res.body).toEqual({id: "1", username: 'test-user', photoUrl: '/some-image.jpg', caption: 'new image', tags: ['one', 'two', 'three']})
+      })
 
   })
 });
